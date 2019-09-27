e57a2a5bf29a3eef68e02605a04cdfaedfc9f148
Cydney <johnson_cydney@yahoo.com>
2019-09-26 21:24:04 -0500

    cyd

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -1,22 +1,38 @@
+
 package org.launchcode.controllers;
 
+
+
 import org.launchcode.models.JobData;
+
 import org.springframework.stereotype.Controller;
+
 import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.bind.annotation.RequestParam;
 
+
+
 import java.util.ArrayList;
+
 import java.util.HashMap;
 
+
+
 /**
+
  * Created by LaunchCode
+
  */
+
 @Controller
 
 @RequestMapping(value = "search")
 
 public class SearchController {
+
 
 
     @RequestMapping(value = "")
@@ -28,6 +44,8 @@ public class SearchController {
         return "search";
 
     }
+
+
 
     @RequestMapping(value = "results")
 
@@ -60,3 +78,5 @@ public class SearchController {
 
 
     }
+
+}
